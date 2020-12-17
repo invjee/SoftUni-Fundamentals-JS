25c32feaab092ed281d0cbb05be36ffd7e244ae3
invjee <74527956+invjee@users.noreply.github.com>
2020-12-18 01:27:24 +0200

    Add files via upload

---
+++ Fundamentals JS/Data Types Lab/09TriplesLetteres.js
@@ -0,0 +1,17 @@
+function tripplesLet(n){
+    let letter = String.fromCharCode(97)
+    //console.log(letter)
+    for(let a = 0; a<n; a++){
+        let letterA = String.fromCharCode(97+a)
+        for(let b = 0; b < n; b++){
+            let letterB = String.fromCharCode(97+b)
+            for(let c = 0; c< n; c++){
+                let letterC = String.fromCharCode(97+c)
+                console.log(letterA+letterB+letterC)
+            }
+        }
+    }
+
+
+}
+tripplesLet(3)
