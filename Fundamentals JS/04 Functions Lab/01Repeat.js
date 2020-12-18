7090042331a5f59a6aa236241d143badbcbaa516
invjee <74527956+invjee@users.noreply.github.com>
2020-12-18 02:00:27 +0200

    Create 01Repeat.js

---
+++ Fundamentals JS/04 Functions Lab/01Repeat.js
@@ -0,0 +1,15 @@
+function repeat(string, num){
+    let newstring = ''
+    for(let i = 0; i< num; i++){
+        newstring += string
+
+        
+    }
+    console.log(newstring)
+
+
+
+}
+repeat('abc',
+    3
+    )
