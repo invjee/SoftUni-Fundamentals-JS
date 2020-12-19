2d5a00f9ae5d45cae9ee5b94e9abb3a187f9ebf7
Velina Surlekova <74527956+invjee@users.noreply.github.com>
2020-12-19 21:30:47 +0200

    Create 01Name.js

---
+++ Fundamentals JS/06ObjectsAndClassesLab/01Name.js
@@ -0,0 +1,14 @@
+function person(firstName, lastName, age){
+    let person = {
+        firstName,
+        lastName,
+        age,
+    }
+
+    return person
+
+}
+person("Peter", 
+"Pan",
+"20"
+)
